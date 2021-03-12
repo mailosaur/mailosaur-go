a3110e87b6b619a59ceb400e443e2815114d7cb8
Jon Mahoney <jon@mailosaur.com>
2021-03-12 19:37:59 +0000

    Version bump

--- client.go
+++ client.go
@@ -39,7 +39,7 @@ func NewWithClient(apiKey string, httpClient *http.Client) *MailosaurClient {
 		baseUrl: "https://mailosaur.com/",
 		apiKey:  apiKey,
 		httpClient: httpClient,
-        userAgent: "mailosaur-go/0.1.0",
+        userAgent: "mailosaur-go/0.2.0",
 	}
 
 	c.Servers = &ServersService{client: c}
